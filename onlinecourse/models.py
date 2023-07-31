87c87f19cf7d189568428e64306a6252caf6a0dd
ravula7 <ravula7@ad.unc.edu>
2023-07-31 14:21:24 -0400

    task 1 done

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -128,7 +128,7 @@ class Question(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
- class Choice(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
