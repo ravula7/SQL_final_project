08e1080b8b8377bcc7a1b2ef4001fcc0fbd3f428
ravula7 <ravula7@ad.unc.edu>
2023-08-01 10:06:40 -0400

    finished task 3

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -110,7 +110,8 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id)))
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
